30a90a7e798c1af0f25214e5c94c489140da6dcf
Dmitriy <klimov.dima@mail.ru>
2022-07-13 00:38:01 +0300

    Нахождение элементов в массиве

--- Example011/Program.cs
+++ Example011/Program.cs
@@ -1,71 +1,41 @@
-﻿
-void FillArray(int[] collection)
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
     {
-        int length = collection.Length;
-        int index = 0;
-        while (index < length)
-        {
-            collection[index] = new Random().Next(1, 10);
-            index++;
-        }
+        collection[index] = new Random().Next(1, 10);
+        index++;
     }
+}
 void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
     {
-        int count = col.Length;
-        int position = 0;
-        while (position < count)
-        {
-            Console.WriteLine(col[position]);
-            position++;
-        }
+        Console.WriteLine(col[position]);
+        position++;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 int indexOf(int[] collection, int find)
 {
     int count = collection.Length;
     int index = 0;
-    int position=0;
+    int position = -1;
 
-    while (index<count)
+    while (index < count)
     {
-        if(collection[index] == find)
+        if (collection[index] == find)
         {
             position = index;
+            break;
         }
         index++;
-    return position; 
+    }
+    return position;
 }
-return position; 
 
 int[] array = new int[10];
 
@@ -73,5 +43,5 @@ FillArray(array);
 PrintArray(array);
 Console.WriteLine();
 
-int pos = indexOf(array, 4);
+int pos = indexOf(array, 444);
 Console.WriteLine(pos);
