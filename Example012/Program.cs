97508172a57825226ec2904154e8fe89df36b567
Dmitriy <klimov.dima@mail.ru>
2022-07-18 21:37:39 +0300

    Цикл for

--- Example012/Program.cs
+++ Example012/Program.cs
@@ -1,21 +1,22 @@
 ﻿// Вид 1
 
-void Method1 ()
+void Method1()
 {
     Console.WriteLine("Автор...");
 }
+
 //Method1();
 
 // Вид 2
 
-void Method2 (string msg)
+void Method2(string msg)
 {
     Console.WriteLine(msg);
 }
 
 //Method2(msg: "Текст сообщения");
 
-void Method21 (string msg, int count)
+void Method21(string msg, int count)
 {
     int i = 0;
     while (i < count)
@@ -39,15 +40,29 @@ void Method21 (string msg, int count)
 
 // Вид 4
 
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "asdf");
+// Console.WriteLine(res);
+
 string Method4(int count, string text)
 {
-    int i=0;
     string result = String.Empty;
-    while (i<count)
+    for (int i = 0; i < count; i++)
     {
         result = result + text;
-        i++;
     }
+
     return result;
 }
 
